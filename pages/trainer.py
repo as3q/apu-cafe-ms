cd59aec2cce1f7101feaa4e3e9f5006f6589b0d5
Mahmood Mohammed Qaid <128432141+as3q@users.noreply.github.com>
2024-02-25 20:39:46 +0800

    Final trainer v7.3

--- pages/trainer.py
+++ pages/trainer.py
@@ -1,169 +1,180 @@
 def trainerPage(user, data, dataUpdater):
 
-#     #db = open("data.txt", "r")
-#     #data = json.load(db)
-#     #print("trainer")
-    def admin_menu_choice(user, data, dataUpdater):
-        
-        state= True
-        while state == True:
-            choise= input("1.Add class info \n 2. Update and delete class info \n 3. View list of students\n 4.Send feedback \n 5. Exit")
-            match choise:
-                case "1":
-                    Add_class_info(user, data, dataUpdater)  
-                case "2":
-                    Update_delete_class_info(data)
-                case"3":
-                    View_list_students()
-                case "4":
-                    Send_feedback()
-                case "5":
-                    state= False
-                    break
-                case other:
-                    print("Invalid choise, please chose form the given choises")
-                    return
-                
-# Adding coaching info code
-    def Add_class_info(user, data, dataUpdater):
-        module_name = None
-        fee = None
-        strtclass = None
-        level = None
-        
-        print("Adding coaching class information")
-        module_name = input("Enter programming module: ")
-        fee = input("Enter the fee: RM")
-        strtclass = input("Enter the starting date (yyyy-mm-dd): ")
-        level = input("Enter level: ") # Beginner - Intermediate - Advanced #
-                    
-        newmodule_id=f"{module_name.lower()[:2]}{level[:3].lower()}{strtclass[5:7]}"
-        
-        new_class = {
-        "module_id": newmodule_id,
-        "module_name": module_name,
-        "module_level": level,
-        "trainer_tp": None,
-        "students_enrolled": [],
-        "module_starting_date": strtclass,
-        "module_fee": fee
-        }
-        
-        data["modules_data"].append(new_class)
-        
-        dataUpdater(data)
-        print("class added successfully!")
-                
-                        
+    def trainer_menu_choice(user, data, dataUpdater):
+        while True:
+            print("1. Add class info\n2. Update and delete class info\n3. View list of students\n4. Send feedback\n5. Exit")
+            choice = input("Enter your choice: ")
 
-#    #adding cource info
-        
-#    #########showing the modules to the trainer
-#         for modules in data["modules_data"] :
-#                 print(f"""                  
-#                           module name: {modules["module_name"]}
-#                           module leve:{modules["module_level"]}
-#                           module starting: {modules["module_starting_date"]}
-#                           module fee: {modules["module_fee"]}\n""")
-
-
-  ########updatin and deleting 
-    def Update_delete_class_info(data):
-        choise4= input('Enter the module ID of the module')
-        for update in data["modules_data":]:
-            if choise4==update["module_id"]:
-                print("Updating and deleting coaching class info")
-                state1= True
-                while state1 == True:
-                    choise2= input("1. Update \n 2.Delete")
-                    match choise2:
-                        case"1":
-                            update_info(data, dataUpdater)
-                        case "2":
-                             deleting()
-                        case "3":
-                            state1= False
-                        case other:
-                            print("Invalid choise, please chose form the given choises, choose from the given number please")
+            if choice == "1":
+                Add_class_info(user, data, dataUpdater)
+            elif choice == "2":
+                Update_delete_class_info(data, dataUpdater)
+            elif choice == "3":
+                View_list_students(data)
+            elif choice == "4":
+                Send_feedback(data, dataUpdater)
+            elif choice == "5":
+                break
             else:
-                print("this TP dose not exixt try again")
-                        
+                print("Invalid choice, please choose from the given options.")
 
-    # updating foncation   
-    def update_info(data , dataUpdater):
-        choise5= input('1.module name\n 2.module level3\n3.module starting data\n4.module fee')
-        if choise5== "module name":
-            update_moudle_name()
-        elif  choise5== "module level":
-            update_module_level()
-    
-        elif choise5== "module starting date":
-            update_moudle_starting_date()
-    
-        elif  choise5== "module fee":
-            update_module_fee()
-        else:
-            print("viled coice")
-    #function for the udateing confunction                
-    def update_moudle_name(dataUpdater , data):
-        newmodulename= input("Enter the new name")
-        newmoduleupdatedname={"module_name":newmodulename}
-        data["module_data"]["module_name"].append(newmoduleupdatedname)
-        dataUpdater(data)
-
-
-    def update_module_level():
-        newmodulelevel= input("Enter the new level")
-        newmoduleupdatelevel={"module_level":newmodulelevel}
-        data["module_data"]["module_level"].append(newmoduleupdatelevel)
-        dataUpdater(data)
-               
-    def update_moudle_starting_date():
-        newdate= input("Enter the new date")
-        newupdatedate={"module_starting_date":newdate}
-        data["module_data"]["module_starting_date"].apppend(newupdatedate)
-        dataUpdater(data)
-
-    def update_module_fee():
-       newfee= input("Enter the new fee")
-       newupdatefee={"module_fee":newfee}
-       data["module_data"]["module_fee"].append(newupdatefee)
-       dataUpdater(data)
-                    
-    #deleteing a module 
-    def deleting():
-       for deletedata in data["modules_data"]:
-        if choise4 == deletedata["module_id"]:
-           data["module_id"].remove(deletedata)
-           dataUpdater(data)
-    
-
-
-    #viewlist of student
-    def View_list_students():
-        for students in data["modules_data"]:
-            print(students["students_enrolled"])
-            input("unter SHIFT to go to the menu")
-        
-    
-    ##send feedback
-    def Send_feedback():
-        new_message= input("Enter a massage\n")
-    
-        ids=[]
-        for feedback in data["trainer_feedback"]:
-            ids.append(feedback["message_id"])
+    def Add_class_info(user, data, dataUpdater):
+        module_name = input("Enter name of the module: ")
+        fee = input("Enter the fee: ")
+        strtclass = input("Enter the starting date (yyyy-mm-dd): ")
+        while True:
+            level = input("1.Beginner\n2.Intermediate\n3.Advanced")
+            if level == "1":
+                    level="Beginner"
+                    break
+            if level == "2":
+                    level="Intermediate"
+                    break
+            if level == "2":
+                    level="Advanced"
+                    break
+            else:
+                print("valied choice")
             
-            new_id= int(max(ids))+1
-            
-            new_feedback= {"massage=id":new_id,
-                        "author_tp": user["user_tp"],
-                        "message":new_message}
-                        
-        data["trainer_feedback"].append(new_feedback)     
-        
+
+        newmodule_id = f"{module_name.lower()[:2]}{level[:3].lower()}{strtclass[5:7]}"
+
+        new_class = {
+            "module_id": newmodule_id,
+            "module_name": module_name,
+            "module_level": level,
+            "trainer_tp": user["user_tp"],
+            "students_enrolled": [],
+            "module_starting_date": strtclass,
+            "module_fee": fee
+        }
+
+        data["modules_data"].append(new_class)
+
         dataUpdater(data)
-    admin_menu_choice(user, data, dataUpdater)
-    
-    
-# write your code inside the function
+        print("Class added successfully!")
+
+    def Update_delete_class_info(data, dataUpdater):
+        # Display Modules #
+        for modules in data["modules_data"]:
+            print(f"""
+                    module ID  : {modules["module_id"]}                  
+                    module name: {modules["module_name"]}
+                    module level: {modules["module_level"]}
+                    module starting: {modules["module_starting_date"]}
+                    module fee: {modules["module_fee"]}\n""")
+
+        # Prompt for module #
+        while True:
+            choice4 = input('Enter the module ID of the module: ')
+
+            module_found = next((update for update in data["modules_data"] if choice4 == update["module_id"]), None)
+            if module_found:
+                # Module ID exists, you can break the loop
+                break
+            else:
+                print("This module ID does not exist. Please try again.")
+            continue
+                   
+       
+
+                
+
+            # Choose Delete/Update #
+        while True:
+            print("1. Update\n2. Delete\n3. Exit")
+            choice2 = input("Enter your choice: ")
+
+            if choice2 == "1":
+                update_info(choice4, data, dataUpdater)
+            elif choice2 == "2":
+                confirm_delete = input("Are you sure you want to delete this module? (y/n): ").lower()
+                if confirm_delete == "y":
+                    deleting(choice4, data, dataUpdater)
+                    break
+                elif confirm_delete == "n":
+                    print("Deletion canceled.")
+                    break
+                else:
+                    print("Invalid choice. Please enter 'y' or 'n'.")
+            elif choice2 == "3":
+                return
+            else:
+                print("Invalid choice, please choose from the given options.")
+
+    def update_info(choice4, data, dataUpdater):
+        while True:
+            print("1. Module starting date\n2. Module fee\n3. Exit")
+            choice5 = input("Enter your choice: ")
+
+            if choice5 == "1":
+                update_module_starting_date(choice4, data, dataUpdater)
+            elif choice5 == "2":
+                update_module_fee(choice4, data, dataUpdater)
+            elif choice5 == "3":
+                return
+            else:
+                print("Invalid choice")
+
+    def update_module_starting_date(choice4, data, dataUpdater):
+        newdate = input("Enter the new date: ")
+        for update_module in data["modules_data"]:
+            if choice4 == update_module["module_id"]:
+                update_module["module_starting_date"] = newdate
+                update_module_id(update_module)
+                break
+
+        dataUpdater(data)
+
+    def update_module_fee(choice4, data, dataUpdater):
+        newfee = input("Enter the new fee: ")
+        for update_module in data["modules_data"]:
+            if choice4 == update_module["module_id"]:
+                update_module["module_fee"] = newfee
+                update_module_id(update_module)
+                break
+
+        dataUpdater(data)
+
+    def update_module_id(module):
+        module["module_id"] = f"{module['module_name'].lower()[:2]}{module['module_level'][:3].lower()}{module['module_starting_date'][5:7]}"
+
+    def deleting(choice4, data, dataUpdater):
+        for delete_module in data["modules_data"]:
+            if choice4 == delete_module["module_id"]:
+                data["modules_data"].remove(delete_module)
+                dataUpdater(data)
+                break
+
+    def View_list_students(data):
+         while True:
+            for students in data["modules_data"]:
+                for studentdata in students["students_enrolled"]:
+                    print(f"student TP:{studentdata["student_tp"]}\nStudent enrolled date:{studentdata["enrollment_date"]}")
+            
+            gettingout=input("Enter B to go back: ").lower()
+            if gettingout =="b":
+                break
+            
+            
+        
+
+    def Send_feedback(data, dataUpdater):
+        new_message = input("Enter a message:\n")
+
+        ids = [feedback["message_id"] for feedback in data["trainer_feedback"]]
+        new_id = str(int(max(ids)) + 1)
+
+        new_feedback = {
+            "message_id": new_id,
+            "author_tp": user["user_tp"],
+            "message": new_message
+        }
+
+        data["trainer_feedback"].append(new_feedback)
+        dataUpdater(data)
+
+    trainer_menu_choice(user, data, dataUpdater)
+
+# Call the function
+# trainerPage(user_data, your_data, your_data_updater_function)
